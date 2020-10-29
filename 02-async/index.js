3d0cc33fa47e2788df0401322f2c60fa363e594e
Jongkeun Kim <kjkandrea@gmail.com>
2020-10-29 21:09:42 +0900

    Add sync readfile

--- 02-async/index.js
+++ 02-async/index.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
 
 const data = fs.readFileSync('./data.txt', 'utf-8')
+const syncData = fs.readFile('./data.txt', 'utf-8', (err, data) => {
+  console.log(data + ' sync')
+})
 
-console.log(data)
+console.log(data + ' async')
