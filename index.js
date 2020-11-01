c301bc907582c1d6e5aac2d84c8661cb1b5fe020
Jongkeun Kim <kjkandrea@gmail.com>
2020-11-01 14:19:03 +0900

    Add testcase : GET userId 200 green

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ app.get('/users', (req, res) => {
   res.json(users.slice(0, limit))
 })
 
+app.get('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const user = users.filter((user) => user.id === id)[0]
+
+  res.json(user)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
