2f1131fb0975640825d9188708c040f41ed0904f
Jongkeun Kim <kjkandrea@gmail.com>
2020-11-01 14:36:07 +0900

    Add testcode : GET userId 400 green

--- index.js
+++ index.js
@@ -14,13 +14,20 @@ app.use(morgan('dev'))
 app.get('/users', (req, res) => {
   req.query.limit = req.query.limit || 10
   const limit = parseInt(req.query.limit, 10)
+
   if (Number.isNaN(limit)) return res.status(400)
+
   res.json(users.slice(0, limit))
 })
 
 app.get('/users/:id', (req, res) => {
   const id = parseInt(req.params.id, 10);
-  const user = users.filter((user) => user.id === id)[0]
+
+  if (Number.isNaN(id)) return res.status(400)
+
+  const user = users.filter((user) => user.id === id) [0]
+
+  if (!user) return res.status(404)
 
   res.json(user)
 })
