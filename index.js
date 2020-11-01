3834ea7a0c1581658ae393b01bd939d945071dee
Jongkeun Kim <kjkandrea@gmail.com>
2020-11-01 15:15:29 +0900

    Refactor done() end()

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/users', (req, res) => {
   req.query.limit = req.query.limit || 10
   const limit = parseInt(req.query.limit, 10)
 
-  if (Number.isNaN(limit)) return res.status(400)
+  if (Number.isNaN(limit)) return res.status(400).end()
 
   res.json(users.slice(0, limit))
 })
@@ -23,11 +23,11 @@ app.get('/users', (req, res) => {
 app.get('/users/:id', (req, res) => {
   const id = parseInt(req.params.id, 10);
 
-  if (Number.isNaN(id)) return res.status(400)
+  if (Number.isNaN(id)) return res.status(400).end()
 
   const user = users.filter((user) => user.id === id) [0]
 
-  if (!user) return res.status(404)
+  if (!user) return res.status(404).end()
 
   res.json(user)
 })
