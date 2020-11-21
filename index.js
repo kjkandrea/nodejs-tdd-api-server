d8c9769c8e71f9fa5507a509454620c568ffbabc
Jongkeun Kim <kjkandrea@gmail.com>
2020-11-21 16:49:25 +0900

    API 폴더 구조 이사

--- index.js
+++ index.js
@@ -2,80 +2,14 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const user = require('./api/user')
 const port = 3000
-
-let users = [
-  {id: 1, name:'andrea'},
-  {id: 2, name:'yusso'},
-  {id: 3, name:'alice'}
-]
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/users', (req, res) => {
-  req.query.limit = req.query.limit || 10
-  const limit = parseInt(req.query.limit, 10)
-
-  if (Number.isNaN(limit)) return res.status(400).end()
-
-  res.json(users.slice(0, limit))
-})
-
-app.get('/users/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-
-  if (Number.isNaN(id)) return res.status(400).end()
-
-  const user = users.filter((user) => user.id === id) [0]
-
-  if (!user) return res.status(404).end()
-
-  res.json(user)
-})
-
-app.delete('/users/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10)
-
-  if (Number.isNaN(id)) return res.status(400).end()
-
-  users = users.filter((user) => user.id !== id)
-  res.status(204).end()
-})
-
-app.post('/users', (req, res) => {
-  const name = req.body.name
-
-  if (!name) return res.status(400).end()
-  
-  const isDuplicate = users.some(user => user.name === name)
-  
-  if (isDuplicate) return res.status(409).end()
-
-  const id = Date.now()
-  const user = {id, name}
-  users.push(user)
-  res.status(201).json(user)
-})
-
-app.put('/users/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10)
-  if (Number.isNaN(id)) return res.status(400).end()
-
-  const name = req.body.name
-  if (!name) return res.status(400).end()
-
-  const isDuplicate = users.some(user => user.name === name)
-  if (isDuplicate) return res.status(409).end()
-
-  const user = users.filter(user => user.id === id)[0]
-  if (!user) return res.status(404).end()
-
-  user.name = name
-
-  res.json(user)
-})
+app.use('/users', user)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
